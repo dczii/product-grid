6bca161436531b2daaa03dad9d75903648c0afc1
Zii <dczii@live.com>
2017-09-29 09:16:54 -0600

    set-up api connection

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
-# React Template with superagent for GH Pages
-
-Template created for starting a static website mainly for GitHub Pages but can be used for other purposes.
-
-#Deployment
-`npm run build` will run prior to deploy
-
-### `npm run deploy`
+## Creatella Test by DcZII
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import SearchWebIcon from 'mdi-react/SearchWebIcon';
+import Animate from 'react-animate.css'
+
+import superagent from 'superagent';
+import jsonp from 'superagent-jsonp';
+import { Row, Col } from 'react-grid-system';
 import Header from './Header';
 import Footer from './Footer';
-import { Row, Col } from 'react-grid-system';
+import request from 'superagent';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        results: []
+    };
+  }  
+
+
+  handleSearch() {
+        superagent.get('http://localhost:3000/products')
+            .end((error, response) => {
+               if (error) {
+                   console.error(error);
+               } else {
+                   this.setState({ results: response.body });
+               }
+            });
+    }
+
   render() {
     return (
         <Row className="center">
             <Header />
             <Col md={12} >
                 React Application HomePage
+                <RaisedButton label='test' onClick={this.handleSearch.bind(this)}/>
+
             </Col>
             <Footer />
         </Row>
