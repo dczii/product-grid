f03f8222395c5d10f3e6fc879c28999620d770e4
Zii <dczii@live.com>
2017-09-29 11:54:57 -0600

    Apply formatting of fields.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import RaisedButton from 'material-ui/RaisedButton';
-import SearchWebIcon from 'mdi-react/SearchWebIcon';
-import Animate from 'react-animate.css'
-
 import superagent from 'superagent';
 import jsonp from 'superagent-jsonp';
 import { Row, Col } from 'react-grid-system';
+import SearchWebIcon from 'mdi-react/SearchWebIcon';
+import _ from 'lodash';
+import moment from 'moment';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+
 import Header from './Header';
 import Footer from './Footer';
-import request from 'superagent';
 
 class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        results: []
-    };
-  }  
+    constructor(props) {
+        super(props);
+        this.state = {
+            results: []
+        };
+    }  
 
-
-  handleSearch() {
-        superagent.get('http://localhost:3000/products')
+    componentDidMount() {
+        this.handleSearch();
+    }
+    handleSearch() {
+        superagent.get('http://localhost:3000/products?_page=1&_limit=20')
             .end((error, response) => {
                if (error) {
                    console.error(error);
@@ -36,9 +39,43 @@ class Home extends Component {
         <Row className="center">
             <Header />
             <Col md={12} >
-                React Application HomePage
-                <RaisedButton label='test' onClick={this.handleSearch.bind(this)}/>
-
+                Creatella Products
+                <Row className='grid-header-container'>
+                    <Col xs={3} className='grid-header'>
+                        Date
+                    </Col>
+                    <Col xs={3} className='grid-header'>
+                        Face
+                    </Col>
+                    <Col xs={3} className='grid-header'>
+                        Price
+                    </Col>
+                    <Col xs={3} className='grid-header'>
+                        Size
+                    </Col>
+                </Row>
+                    {this.state.results ? 
+                        _.map(this.state.results, (val) => {
+                            return(<Row className='grid-body-container' key={val.id}>
+                                <Col xs={3}>
+                                    { moment(val.date).isBefore(moment().subtract(7, 'days')) ?
+                                        moment(val.date).format("MMM Do YY")
+                                    :
+                                        moment(val.date).startOf('day').fromNow()
+                                    }
+                                </Col>
+                                <Col xs={3} style={{fontSize:val.size}}>
+                                    {val.face}
+                                </Col>
+                                <Col xs={3}>
+                                    {'$' + val.price.toFixed(2)}
+                                </Col>
+                                <Col xs={3}>
+                                    {val.size}
+                                </Col>
+                            </Row>);
+                        })
+                    : ''}
             </Col>
             <Footer />
         </Row>
